62665d56da7eef8419677d933369f92a8649e405
Jose Marchena <jose.marchena-roldan@iesruizgijon.com>
2023-05-19 14:00:21 +0200

    Añadida clase 1 y sout Hola mundo en principal

--- src/pruebagith/clase1.java
+++ src/pruebagith/clase1.java
@@ -9,13 +9,6 @@ package pruebagith;
  *
  * @author José Marchena
  */
-public class PruebaGitH {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo.");
-    }
+public class clase1 {
     
 }
